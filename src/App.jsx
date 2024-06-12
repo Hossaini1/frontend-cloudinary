4b57f3e060bd4826fa73ffb77432fbeb8371af93
Hossaini <1989hossaini1@gmail.com>
2024-06-12 16:00:26 +0200

    fronend für die cloudinary wurde erstellt

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import NavComponent from './components/NavComponent'
+import CreateProduct from './components/CreateProduct';
+
+
+function App() {
+
+
+  return (
+    <div>
+      <NavComponent />
+      <div className="container mx-auto p-5 lg:w-2/3 lg:p-2">
+        <CreateProduct />
+      </div>
+    </div>
+  );
+}
+
+export default App
