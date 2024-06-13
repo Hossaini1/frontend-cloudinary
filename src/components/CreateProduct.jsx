29b1552dd615db073d5d854b0427e61bb7421c2e
Hossaini <1989hossaini1@gmail.com>
2024-06-13 14:25:22 +0200

    dotenv wurde in frontend hinzugefügt

--- src/components/CreateProduct.jsx
+++ src/components/CreateProduct.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import 'dotenv/config'
 
 const CreateProductForm = () => {
-  const baseUrl = "http://localhost:3000";
+
+  // const baseUrl = "http://localhost:3000";
+
+  const baseUrl = process.env.API_URL;
 
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
